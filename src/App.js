a9aaf967cef7b14325dee1055a4c10256144a4da
pierluigi <pierluigibaiano@gmail.com>
2021-12-02 10:55:35 +0100

    create a form and use state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import './App.scss';
+import { useState } from "react";
+import "./App.scss";
+
+
+
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="App">
-		Hello
+      <form>
+        <fieldset>
+          <legend>Login</legend>
+          <div className="row">
+            <label htmlFor="username">Name</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={handleUsername}
+            />
+          </div>
+          <div className="row">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              onChange={handlePassword}
+              value={password}
+            />
+          </div>
+          <div className="buttonRow">
+            <button onClick={handleLoginButton}>Login</button>
+          </div>
+        </fieldset>
+      </form>
     </div>
   );
 }
